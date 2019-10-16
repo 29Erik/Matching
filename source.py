58a48275e840cb3b1d47e2ab02883cb8306240c0
Sony <e.romero@bees2biz.com>
2019-10-16 18:01:40 -0500

    Erikson001-Primer acercamiento

--- source.py
+++ source.py
@@ -15,8 +15,28 @@ while(True):
   if ret == True: 
     out = cv2.imwrite('Frame.jpg', original)  
     cv2.imshow('Video Original',original)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.waitKey(1)
+    if  0xFF == ord('q'):
       break
+    if 0xFF == 256:
+        # SPACE pressed
+        img_name = "Referencia.jpg".format(img_counter)
+        imgRef=cv2.imwrite(img_name, out)
+        print("Imagen guardada.".format(img_name))
+    
+    img_gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(imgRef,0)
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+      cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
+    cv2.imshow('Resultado',img_rgb)
+    cv2.waitKey(1)
   else:
     break 
 cap.release()
