0d59dde98eaa74679236ec43c71ff0ce7fac851b
Queen <d.reina@bees2biz.com>
2019-10-16 18:13:13 -0500

    Mi perro 2

--- source.py
+++ source.py
@@ -16,26 +16,27 @@ while(True):
     out = cv2.imwrite('Frame.jpg', original)  
     cv2.imshow('Video Original',original)
     cv2.waitKey(1)
+    finded = False
     if  0xFF == ord('q'):
       break
-    if 0xFF == 256:
+    if out%256 == 32:
         # SPACE pressed
-        img_name = "Referencia.jpg".format(img_counter)
-        imgRef=cv2.imwrite(img_name, out)
-        print("Imagen guardada.".format(img_name))
-    
-    img_gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread(imgRef,0)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    w, h = template.shape[::-1]
+        imgRef=cv2.imwrite("Referencia.jpg", out)
+        finded = True
+    if finded:
+      img_gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+      template = cv2.imread(imgRef,0)
+      template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+      w, h = template.shape[::-1]
 
-    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where( res >= threshold)
-    for pt in zip(*loc[::-1]):
-      cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+      res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+      threshold = 0.8
+      loc = np.where( res >= threshold)
+      for pt in zip(*loc[::-1]):
+        cv2.rectangle(out, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+        
 
-    cv2.imshow('Resultado',img_rgb)
+    cv2.imshow('Resultado',out)
     cv2.waitKey(1)
   else:
     break 
