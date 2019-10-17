f6fc905cbf464a8fe7f08c9bb52a70620ee3b2c2
Sony <e.romero@bees2biz.com>
2019-10-16 19:12:52 -0500

    Erikson003-Commitazo

--- source.py
+++ source.py
@@ -16,26 +16,27 @@ while(True):
   if ret == True: 
     out = cv2.imwrite('Frame.jpg', original)  
     cv2.imshow('Video Original',original)
+    out = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     cv2.waitKey(1)
     finded = False
     if  keyboard.is_pressed('q'):
       break
     if keyboard.is_pressed('s'):
         # SPACE pressed
-        imgRef=cv2.imwrite("Referencia.jpg", out)
+        cv2.imwrite("Referencia.jpg", out)
         finded = True
     if finded:
-      img_gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
-      template = cv2.imread(imgRef,0)
-      template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+      template = cv2.imread("Referencia.jpg",0)
+      #template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
       w, h = template.shape[::-1]
 
-      res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+      res = cv2.matchTemplate(out,template,cv2.TM_CCOEFF_NORMED)
       threshold = 0.8
       loc = np.where( res >= threshold)
       for pt in zip(*loc[::-1]):
         cv2.rectangle(out, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
       cv2.imshow('Resultado',out)
+      cv2.imwrite('Resultado.png',out)
       cv2.waitKey(1)
   else:
     break 
